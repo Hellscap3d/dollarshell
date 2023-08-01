1aab1cd58235e95b4d7542fbaa7df42003c2801a
Hellscap3d <72767525+Hellscap3d@users.noreply.github.com>
2023-08-01 11:05:17 -0500

    added help

--- script.py
+++ script.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 import sys,os,subprocess
+if len(sys.argv) == 1:
+	print("dollarshell by Hellscaped")
+	print("Usage: $ <command>")
+	exit(0)
 cmd = ""
 def system(cmd):
 	return subprocess.call('$SHELL -c "$GREPDB"', shell=True, env={'GREPDB': cmd,'SHELL':os.environ["SHELL"]})
